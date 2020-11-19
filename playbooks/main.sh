a6f639dbc99dc84513d1de3a80dfbe6878432a31
root <root@ip-172-31-43-218.eu-central-1.compute.internal>
2020-11-19 07:43:08 +0000

    first commit

---
+++ playbooks/main.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd home/ubuntu/environment/kamal/ansible-demo/inputs/
+#helm install demochat /home/ubuntu/environment/kamal/ansible-demo/inputs/*.tgz
+helm install demochat
+echo 'success'
