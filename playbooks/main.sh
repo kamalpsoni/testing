77b5276a39abf1780a049377e71eaa6fa1db10cb
kamalpsoni <68459102+kamalpsoni@users.noreply.github.com>
2020-11-19 18:44:58 +0530

    Update main.sh

--- playbooks/main.sh
+++ playbooks/main.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
-cd /home/ubuntu/tmp/ && helm install demochat && echo 'success'
+cd /home/ubuntu/tmp/
+tar zxvf demochat-0.1.0.tgz
+helm install demochat --generate-name
+echo 'success'
 
 
